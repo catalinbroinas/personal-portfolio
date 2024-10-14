b6abca8f16be2bde4afc7923f74c09faa02c683f
catalinbroinas <catalinbroinas@yahoo.com>
2024-10-14 22:15:42 +0300

    Add `setupCarousel` function: manage carousel autoplay/pause

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,37 @@ import '../scss/main.scss';
 import { Ripple, Carousel, initMDB } from 'mdb-ui-kit/js/mdb.es.min.js';
 
 function MainDomManager() {
+    const setupCarousel = () => {
+        const introCarousel = document.querySelector('#intro-carousel');
+        const carousel = new Carousel(introCarousel, {
+            ride: false,
+        });
+
+        const next = introCarousel.querySelector('.carousel-control-next');
+        const prev = introCarousel.querySelector('.carousel-control-prev');
+
+        carousel.cycle();
+
+        introCarousel.addEventListener('mouseenter', () => {
+            carousel.pause();
+        });
+
+        introCarousel.addEventListener('mouseleave', () => {
+            carousel.cycle();
+        });
+
+        next.addEventListener('click', () => {
+            carousel.pause();
+        });
+
+        prev.addEventListener('click', () => {
+            carousel.pause();
+        });
+    };
+
     const initApp = () => {
         initMDB({ Ripple, Carousel });
+        setupCarousel();
     };
 
     return { initApp };
