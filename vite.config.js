47aaee86961f04b2a279e8d5f35a641278aaa7bd
catalinbroinas <catalinbroinas@yahoo.com>
2025-09-20 20:24:26 +0300

    chore(config): update root path after moving index.html

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import path from 'path';
 import imagemin from 'vite-plugin-imagemin';
 
 export default {
-    base: './',
-    root: path.resolve(__dirname, 'src'),
+    base: '/',
     server: {
         port: 8080,
         hot: true
     },
     build: {
-        outDir: '../dist',
+        outDir: 'dist',
         emptyOutDir: true,
         rollupOptions: {
             output: {
